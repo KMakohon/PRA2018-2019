dba644ec2750333b74bc4ed9167163905a44213a
KMakohon <kiasarin99@gmail.com>
2018-10-08 22:20:46 +0200

    1.1.1

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String [ ] args) {
         System.out.println("Hello World");
         System.out.println("To żyje!");
+        System.out.println("To żyje!");
 
         logger.info("Welcome to Programming Laboratory");
     }
